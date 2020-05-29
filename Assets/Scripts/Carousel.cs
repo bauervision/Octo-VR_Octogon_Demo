40a994595c7fa378ddca71730ffd2d9eaafd2131
MCBauer <mikecbauervision@gmail.com>
2020-05-29 11:27:52 -0400

    trying rotations

---
+++ Assets/Scripts/Carousel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carousel : MonoBehaviour
+{
+    public float rotationSpeed = 20f;
+
+    private void OnMouseDrag()
+    {
+        float rotY = Input.GetAxis("Mouse Y") * rotationSpeed * Mathf.Deg2Rad;
+        transform.Rotate(-Vector3.up, rotY);
+
+    }
+}
