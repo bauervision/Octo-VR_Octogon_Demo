f9cfb71d60e1cd2c3b866af81881707f160144f7
Mike Bauer <mikecbauervision@gmail.com>
2020-05-29 20:19:04 +0000

    working rotation scale

--- Assets/Scripts/Carousel.cs
+++ Assets/Scripts/Carousel.cs
@@ -1,15 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Carousel : MonoBehaviour
 {
-    public float rotationSpeed = 20f;
 
-    private void OnMouseDrag()
-    {
-        float rotY = Input.GetAxis("Mouse Y") * rotationSpeed * Mathf.Deg2Rad;
-        transform.Rotate(-Vector3.up, rotY);
+public float rotationSpeed = 100f;
+  private Vector3 prevPos = Vector3.zero;
+   private Vector3 curPos = Vector3.zero;
 
-    }
+
+private void Update() {
+      
+    //if(Input.GetMouseButtonDown(0)){
+      
+
+        float x = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        
+        GetComponent<Rigidbody>().AddTorque(Vector3.up * x);
+   // }
+   
 }
+
+//   private Vector3 prevPos = Vector3.zero;
+//    private Vector3 curPos = Vector3.zero;
+//    private bool isDragging = false;
+
+//     private void OnMouseDrag() {
+//         isDragging = true;
+//     }
+
+//    private void Update() {
+//        if(Input.GetMouseButton(0)){
+//            curPos = Input.mousePosition - prevPos;
+//            transform.Rotate(transform.up, Vector3.Dot(curPos, Camera.main.transform.right) * 5f, Space.World);
+//        }
+//        prevPos = Input.mousePosition;
+//        GetComponent<Rigidbody>().AddTorque(Vector3.down * 3);
+//    }
+
+}
+
---
+++ Assets/Scripts/IconMaster.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System;
+
+public class IconMaster : MonoBehaviour
+{
+    public GameObject centralPoint;
+
+    private float farSide = 8.8f;
+     private float closeSide =  0.1633289f;
+
+    private void Update() {
+        float dist = Vector3.Distance(centralPoint.transform.position, transform.position);
+        float scalePercentage = ((farSide - dist)/ farSide) ;
+        Debug.Log("distance to farSide = " + (farSide - dist) + " scale % is " + (scalePercentage)  );
+        if(dist > 0.5){
+            float newValue = System.Convert.ToSingle(transform.localScale.x - (scalePercentage * 0.0001));
+            transform.localScale = new Vector3(newValue, newValue, newValue );
+        }
+    }
+
+}
